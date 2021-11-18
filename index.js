1f597444f0089a44fa5328dcd4683eb0cbcf438f
Paul McGeachie <paulmcgeachie@icloud.com>
2021-11-18 16:39:50 +0000

    finish JS

--- index.js
+++ index.js
@@ -2,3 +2,25 @@
 
 const list = document.querySelector("ul");
 const input = document.querySelector("input");
+const btn = document.querySelector("button");
+
+btn.onclick = () => {
+  let btnVal = input.value;
+  input.value = "";
+
+  const listItem = document.createElement("li");
+  const spanEl = document.createElement("span");
+  const newBtn = document.createElement("button");
+
+  listItem.appendChild(spanEl);
+  spanEl.textContent = btnVal;
+  listItem.appendChild(newBtn);
+  newBtn.textContent = "Delete";
+  list.appendChild(listItem);
+
+  newBtn.onclick = (e) => {
+    list.removeChild(listItem);
+  };
+
+  input.focus();
+};
